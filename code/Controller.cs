8721af6bb98d18a873f9d46c5be02d18e1b15c63
EdigaHe <edigahe@gmail.com>
2019-11-27 19:05:58 -0800

    Codebase Created

---
+++ code/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Rhino.DocObjects;
+
+namespace EnergyPlugin
+{
+    public interface IControllerModelObserver
+    {
+
+    }
+
+
+    public interface Controller
+    {
+        #region default functions
+        void AddRandomGeometry();
+        void ExportModel();
+        #endregion
+
+    }
+
+    public class IncController : Controller
+    {
+        RhinoModel rhinoModel;
+        View view;
+
+        public IncController(View v,  RhinoModel rm)
+        {
+            view = v;
+            rhinoModel = rm;
+            view.setController(this);
+        }
+
+        public void AddRandomGeometry()
+        {
+            rhinoModel.AddRandomGeometry();
+        }
+
+        public void ExportModel()
+        {
+            rhinoModel.ExportModel();
+        }
+    }
+}
---
+++ code/UI/3DPEnergyPanel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Rhino.UI;
+using System.Xml;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+using System.Drawing.Drawing2D;
+using System.IO;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+
+namespace EnergyPlugin
+{
+    public partial class EnergyPluginControl : MetroFramework.Controls.MetroUserControl, View,  IControllerModelObserver
+    {
+
+        #region Initialization & Construction
+        Controller controller;
+        public void setController(Controller cont)
+        {
+            controller = cont;
+            
+            if (EventWatcherHandlers.Instance.IsEnabled == false)
+            {
+                EventWatcherHandlers.Instance.Enable(true);
+                EventWatcherHandlers.Instance.setRhinoModel(ref controller);
+            }
+        }
+
+
+        public EnergyPluginControl()
+        {
+
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = Color.FromArgb(255, 255, 255, 255);
+
+        }
+        #endregion
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            controller.ExportModel();
+        }
+
+        private void AddShapeBtn_Click(object sender, EventArgs e)
+        {
+            controller.AddRandomGeometry();
+        }
+    }
+}
