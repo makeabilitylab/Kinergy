1cd4c12eb7619fa14f4f08fa5540c567bd32eeb4
SuXia0919 <su_xia1997@foxmail.com>
2022-03-24 22:20:10 -0700

    Added reused code for the rest 3 KUs

--- Code/KinergyApp/KinergyApp/KineticUnits/IntermittentRotation/IntermittentRotationModule.cs
+++ Code/KinergyApp/KinergyApp/KineticUnits/IntermittentRotation/IntermittentRotationModule.cs
@@ -13,7 +13,7 @@ using HumanUIforKinergy.KinergyUtilities;
 using Kinergy.Geom;
 using System.Linq;
 
-namespace HumanUIforKinergy.KineticUnits.IntermittentRotation
+namespace InterRotation
 {
     public class IntermittentRotationModule : GH_Component
     {
@@ -28,7 +28,7 @@ namespace HumanUIforKinergy.KineticUnits.IntermittentRotation
         int strokeLevel;    // value of the stroke number
         int energyLevel;
         Vector3d direction;             // kinetic unit direction
-        InstantRotation motion;
+        IntermittentRotation motion;
         List<Arrow> lockDirCandidates;
         Arrow p;
 
@@ -65,7 +65,45 @@ namespace HumanUIforKinergy.KineticUnits.IntermittentRotation
         bool testPreBtn;
         bool testBakeBtn;
         int motionControlMethod; // 1: press; 2: turn
+        
+        Helpers helperFun;
+        
+        WarningWin warningwin = new WarningWin();
 
+        Vector3d selectedAxisVector = Vector3d.Unset;
+
+        ObjectAttributes solidAttribute, orangeAttribute, redAttribute, blueAttribute, greenAttribute;
+        int selectedAxisIndex;
+        Guid convertedPortion;
+        List<Brep> brepCut;
+        Guid reserveBrepID1;
+        Guid reserveBrepID2;
+        Point3d motionCtrlPointSelected;
+        Guid ee;
+        Brep eeModel;
+        Point3d eeCircleDotPt = new Point3d();
+        Point3d eeLineDotPt = new Point3d();
+        Vector3d kineticUnitDir = new Vector3d();
+        Vector3d axelDir = new Vector3d();
+        Point3d eeCenPt = new Point3d();
+        double finalGearPositionRatio;
+
+        Guid mainAxisArrow = Guid.Empty;
+        Guid perpAxisArrowUp = Guid.Empty;
+        Guid perpAxisArrowDown = Guid.Empty;
+        int eeMovingDirectionSelection = 0;//-1 for error, 0 for unset, 1 for main, 2 for perp up, 3 for perp down
+        GearTrainParam selectedGearTrainParam;
+
+        double axelSpace = 0;
+        double gearSpace = 0;
+
+        List<Entity> axel_spacer_entities = new List<Entity>();
+        List<Gear> gears = new List<Gear>();
+        List<Entity> spring_entities = new List<Entity>();
+        //List<GearParameter> gear_info = new List<GearParameter>();
+        List<Point3d> lockPos = new List<Point3d>();
+        bool spiralLockNorm = false;
+        Vector3d spiralLockDir = new Vector3d();
         /// <summary>
         /// Initializes a new instance of the IntermittentRotationModule class.
         /// </summary>
@@ -290,17 +328,215 @@ namespace HumanUIforKinergy.KineticUnits.IntermittentRotation
 
             if (toSelectRegion)
             {
+                if (selObjId != Guid.Empty)
+                {
+                    myDoc.Objects.Show(selObjId, true);
+                    myDoc.Views.Redraw();
+                    toBeBaked.Clear();
+                    selObjId = Guid.Empty;
+                }
 
+                ObjRef objSel_ref;
+                var rc = RhinoGet.GetOneObject("Select a surface or polysurface", false, ObjectType.AnyObject, out objSel_ref);
+                if (rc == Rhino.Commands.Result.Success)
+                {
+                    #region Pre-process #1: get the selected brep
+
+                    selObjId = objSel_ref.ObjectId;
+                    ObjRef currObj = new ObjRef(selObjId);
+                    myDoc.Objects.Select(currObj);
+
+                    model = currObj.Brep();
+
+                    #endregion
+
+                    #region Pre-process #2: get the ininital inner cavity of the selected brep
+
+                    BoundingBox box = model.GetBoundingBox(true);
+                    box.Inflate(-2.0);
+                    box.Transform(Transform.Scale(box.Center, 2));
+                    //arrowScale = box.Diagonal.Length / 100;
+                    center = box.Center;
+
+                    #endregion
+                }
             }
 
             if (toSetMotionControl)
             {
+                if (model != null)
+                {
+                    //Step 1: select the axis (X, Y, or Z axis)
+                    selectedAxisIndex = new XYZselection(center, myDoc, redAttribute, greenAttribute, blueAttribute, 30).selectedAxis;
 
+                    //region Step 2: drag the two planes to decide the portion
+                    PortionSelection pselect = new PortionSelection(model, selectedAxisIndex, myDoc);
+                    t1 = pselect.t1;
+                    t2 = pselect.t2;
+                    selectedAxisVector = pselect.axis;
+                    direction = selectedAxisVector;
+                    skeleton = pselect.skeleton;
+                    PlaneSelected = true;
+
+
+                    #region Step 3: cut model and calculate the inner cavity
+
+
+                    if (PlaneSelected)
+                    {
+                        brepCut.Clear();
+                        // Call out the waiting window
+                        processingwin.Show();
+
+                        brepCut = Helpers.cutModel(model, skeleton, t1, t2, selectedAxisVector, myDoc);
+                        innerCavity = Helpers.getInnerCavity(brepCut, selectedAxisVector);
+
+
+                        processingwin.Hide();
+
+                        myDoc.Objects.Hide(selObjId, true);
+
+                        reserveBrepID1 = myDoc.Objects.AddBrep(brepCut[0]);
+                        reserveBrepID2 = myDoc.Objects.AddBrep(brepCut[2]);
+
+                        convertedPortion = myDoc.Objects.AddBrep(brepCut[1]);
+                        myDoc.Objects.Select(convertedPortion);
+                    }
+
+                    #endregion
+
+                    //Step 4: ask the user to select which side to add the motion control
+                    motionCtrlPointSelected = new SideSelection(skeleton, myDoc, blueAttribute).motionCtrlPointSelected;
+
+                    #region Step 5: create an instance of Continuous Translation class
+
+                    motion = new IntermittentRotation(model, direction, energy_input, stroke_input, speed_input);
+
+                    motion.Set3Parts(t1, t2, brepCut[0], brepCut[1], brepCut[2]);
+
+                    //toBeBaked.Add(reserveBrepID1);
+                    //toBeBaked.Add(reserveBrepID2);
+                    //myDoc.Views.Redraw();
+                    #endregion
+                }
             }
 
             if (toSetEEPos)
             {
+                // select a direction around a circle on the end-effector's side
+                EndEffectorDirectionSelection directionSelection = new EndEffectorDirectionSelection(brepCut, skeleton, direction, motionCtrlPointSelected, t1, t2, myDoc, redAttribute);
 
+                kineticUnitDir = directionSelection.kineticUnitDir;
+                axelDir = directionSelection.axelDir;
+                eeCenPt = directionSelection.eeCenPt;
+                //Xia's note: moved eeCenPt by 2mm inward.
+
+                //Then select ee model
+                ObjRef objSel_ref1;
+                var rc1 = RhinoGet.GetOneObject("Select the end effector model. ", false, ObjectType.AnyObject, out objSel_ref1);
+                if (rc1 == Rhino.Commands.Result.Success)
+                {
+                    //set up first ee
+                    ee = objSel_ref1.ObjectId;
+                    ObjRef currObj = new ObjRef(ee);
+                    myDoc.Objects.Select(currObj);
+                    eeModel = currObj.Brep();
+                    myDoc.Objects.Hide(ee, true);
+                    myDoc.Views.Redraw();
+                }
+                else
+                {
+                    warningwin.Text = "No end effector model is selected.";
+                    warningwin.Show();
+                }
+                //Use ee position to calculate important parameters
+
+                //TODO 
+
+                //Then generate gears , crank and more. TODO change from CT to IO
+                #region generate the spring motor, transmission mechanism, and the mechanism mating the end-effector
+                Point3d startPt = skeleton.PointAtNormalizedLength(0);
+                Point3d endPt = skeleton.PointAtNormalizedLength(1);
+
+                if (t1 > t2)
+                {
+                    endPt = skeleton.PointAtNormalizedLength(t1);
+                    startPt = skeleton.PointAtNormalizedLength(t2);
+                }
+                else
+                {
+                    startPt = skeleton.PointAtNormalizedLength(t1);
+                    endPt = skeleton.PointAtNormalizedLength(t2);
+                }
+                double unitLenth = startPt.DistanceTo(endPt);
+                //double initialOffset = finalGearPositionRatio * pts[1].DistanceTo(pts[0]);
+                //motion.CalculateSpaceForKineticUnit(kineticUnitDir, axelDir, axelSpace, gearSpace, unitLenth, initialOffset, finalGearPositionRatio);
+
+                // wait for Xia, eeLineDotPt is the point passed to Xia's function 
+                //motion.GenerateGearTrain(finalGearPositionRatio, eeCenPt, speed_input, kineticUnitDir, axelDir);
+
+                // convert the inner cavity brep into box
+                Box innerCavityBox = new Box(innerCavity.GetBoundingBox(true));
+                //Offset inner cavity by 2mm
+                innerCavityBox.Inflate(-2);
+                // gear's facewidth is fixed for our project except for the first gear in the gear train
+                List<GearTrainScheme> gear_schemes = GenerateGearTrain.GetGearTrainSchemes(direction, axelDir, eeLineDotPt, innerCavityBox, 3.6);
+
+                if (gear_schemes.Count == 0)
+                {
+                    // the selected body is too small to embed the kinetic unit
+                    warningwin.Text = "The selected body is too small to add the kinetic unit!";
+                    warningwin.Show();
+                }
+                else
+                {
+                    #region for testing
+                    //List<GearParameter> parameters = gear_schemes[0].parameters[0].parameters;
+                    //for (int i = 0; i < parameters.Count; i++)
+                    //{
+                    //    GearParameter p = parameters[i];
+                    //    Gear newGear = new Gear(p.center, p.norm, p.xDirection, (int)Math.Floor(p.radius * 2), 1, 20, p.faceWidth, 0, false);
+                    //    newGear.Generate();
+
+                    //    myDoc.Objects.AddBrep(newGear.Model);
+                    //    myDoc.Views.Redraw();
+                    //}
+                    #endregion
+
+                    List<double> gr_list = new List<double>();
+                    foreach (GearTrainScheme gts in gear_schemes)
+                    {
+                        foreach (GearTrainParam gtp in gts.parameters)
+                        {
+                            gr_list.Add(gtp.gearRatio);
+                        }
+                    }
+
+                    gr_list.Sort();
+
+                    int schemeNum = -1;
+                    int paramNum = -1;
+
+                    helperFun.mapSpeedToGears(speed_input, gr_list, gear_schemes, out schemeNum, out paramNum);
+
+                    if (paramNum == -1 || schemeNum == -1)
+                    {
+                        return;
+                    }
+                    selectedGearTrainParam = gear_schemes[schemeNum].parameters[paramNum];
+                    //gear_info.Clear();
+                    //gear_info = gear_schemes[schemeNum].parameters[paramNum].parameters;
+
+                    #region generate all the axels and spacers for the gears
+                    axel_spacer_entities = helperFun.genAxelsStoppers(selectedGearTrainParam.parameters, model, motionControlMethod, 0.3);
+                    #endregion
+
+                    #region generate all the gears
+                    gears = helperFun.genGears(selectedGearTrainParam.parameters, motionControlMethod, 0.4);
+                    #endregion
+                    motion.AddGears(gears, axel_spacer_entities, selectedGearTrainParam);
+                }
+                #endregion
             }
 
             if (toSetAxisDir)
